3557cc14a227a47f586a0a1065465296c8f75d6b
sanketsolanke4 <112948356+sanketsolanke4@users.noreply.github.com>
2023-04-20 14:34:42 +0530

    Done

--- index.js
+++ index.js
@@ -1,111 +1,27 @@
-//togg;e icon nvbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = ()=>{
- menuIcon.classList.toggle('fa-times')   
- navbar.classList.toggle('active')  
- 
+function myfunction(){
+    document.getElementById("mytopnav").classList.toggle("responsive");
+    
 }
-
-{/* <i class='bx bx-x' ></i> */}
-// Scroll seaction Active icons
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight
-        let id = sec.getAttribute('id')
-
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
-        }
-    })
-
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100)
-
-    // remove toggle from and nabvbar when click navbar link (scrooll)
-    menuIcon.classList.remove('bx')   
-    navbar.classList.remove('active')  
-
-}
-
-// Scroll reveal
-ScrollReveal({
-    distance: '80px',
-    duration: 2000,
-    delay:200
-})
-ScrollReveal().reveal('.home-content, .heading',  {origin:'top'})
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form',  {origin:'bottom'})
-ScrollReveal().reveal('.home-content h1, .about-img',  {origin:'left'})
-ScrollReveal().reveal('.home-content p, .about-content .slider-thumb .coding1' ,  {origin:'right'})
+//navbar sticky
+window.onscroll = function() {myFunction()};
 
 
-// Typing animation
-const typed = new Typed('.multiple-text', {
-    strings : ['JAVA BACKEND DEVELOPER', 'GAMER', 'FODDIE'],
-    typeSpeed: 100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-})
+var mytopnav = document.getElementById("mytopnav");
 
-var qty = 1200;
+var sticky = mytopnav.offsetTop;
 
-var el = document.querySelector(".odometer");
-
-od = new Odometer({
-  el: el,
-  format: "(,ddd)",
-  theme: "default"
-});
-od.update(1)
-setTimeout(function () {
-  odometer.innerHTML = qty++;
-}, 1000);
-
-
-
-$('.count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 3000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
-});
-
-//email
-
-document.querySelector("#sanket").addEventListener("onClick",funn())
-
-function funn(){
-event.preventDefault();
-    console.log("hii");
-let name=document.getElementById("n1").value;
-let email=document.getElementById("e1").value;
-let message=document.getElementById("y1").value;
-let subject=document.getElementById("s1").value;
-
-    emailjs.send("service_s1pfduq","template_589ejzl",{
-        from_name: name,
-        to_name: email,
-        message: message,
-        reply_to:subject,
-        },"39Y6Fl52z5FGE4dSg");
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    mytopnav.classList.add("sticky")
+  } else {
+    mytopnav.classList.remove("sticky");
+  }
 }
 
 GitHubCalendar(".calendar", "sanketsolanke4");
 // or enable responsive functionality
 GitHubCalendar(".calendar", "sanketsolanke4", { responsive: true });
+
+function resume(){
+  window.open("https://drive.google.com/file/d/1P4yUP7L26RuoF7DKPpQr0KVFFMM8rWVe/view?usp=share_link", "_blank");
+}
